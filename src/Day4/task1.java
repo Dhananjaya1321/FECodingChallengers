c0e874efa2f88f8332b6971cfb2f12dd1747b6c9
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-05-23 21:05:48 +0530

    updated

---
+++ src/Day4/task1.java
@@ -0,0 +1,10 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        System.out.print("Input number : ");
+        System.out.println("Bit count : "+(new Scanner(System.in).nextInt()+"").length());
+    }
+}
